86a8272c5c2e1e36ae4d2f7b3edd2fc49d706469
Vinicius Watanabe <vinicius_watanabe@hotmail.com>
2017-10-12 14:58:25 +0000

    initil commit

---
+++ link_convert_windows_mac.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $(".btn_converter").click(play);
+    
+    function play(){
+        var codigo_link = $(".input_box").val();
+        var codigo_inicio = codigo_link.replace("H:\\", "smb://almap-file/atendimento$/");
+        var pesquisar_codigo = codigo_inicio.search("\\\\");
+        
+        while(pesquisar_codigo > 0){
+            var codigo_final = codigo_inicio.replace("\\", "/");
+            var codigo_inicio = codigo_final;
+            var pesquisar_codigo = codigo_inicio.search("\\\\");
+        }
+        
+        $(".resultado").text(codigo_inicio);
+    }
+});
