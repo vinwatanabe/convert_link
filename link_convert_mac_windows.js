46ed9f27b866c1215b3e94bc43026bf152266bb5
Vinícius Watanabe <vinicius_watanabe@hotmail.com>
2018-11-21 19:25:56 -0200

    Updated ID

--- link_convert_mac_windows.js
+++ link_convert_mac_windows.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
     
     function play(){
         var codigo_link = $(".input_box").val();
-        var codigo_inicio = codigo_link.replace("smb://172.16.32.36/ClientesID/", "I:\");
+        var codigo_inicio = codigo_link.replace("smb://172.16.32.36/ClientesID/", "I:\\");
         var pesquisar_codigo = codigo_inicio.search("/");
         
         while(pesquisar_codigo > 0){
